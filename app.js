4f1bf566f19dea54b790e4ef0c075508f9c5d953
copofo <copofo4243@gmail.com>
2023-06-13 13:00:42 -0300

    9

---
+++ app.js
@@ -0,0 +1,41 @@
+/*
+
+const express = require('express')
+const ytdl = require('ytdl-core')
+const app = express()
+
+
+
+
+
+app.get('/', function (req, res) {
+  const {url} = req.query
+  console.log(url)
+  res.header("Content-Disposition", 'attachmentt; filename="video.mp4"')
+  return ytdl(url, {filter: "videoandaudio"}).pipe(res);
+  
+})
+
+
+
+
+app.listen(3000)
+*/
+
+
+const express = require('express');
+const ytdl = require('ytdl-core');
+const app = express();
+
+app.get('/', function (req, res) {
+  const { url } = req.query;
+  console.log(url);
+  res.header("Content-Disposition", 'attachment; filename="video.mp4"');
+  return ytdl(url, { filter: "videoandaudio" }).pipe(res);
+});
+
+app.listen(3000, function () {
+  console.log('Servidor em execução na porta 3000');
+});
+
+app.use(express.static("pg"))
